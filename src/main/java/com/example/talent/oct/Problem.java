5e20068d8ab950469e74b0c54629bffa80984a6f
ludyi003 <72963268+ludyi003@users.noreply.github.com>
2020-10-16 15:10:27 +0800

    20200106

--- src/main/java/com/example/talent/oct/Problem.java
+++ src/main/java/com/example/talent/oct/Problem.java
@@ -8,6 +8,32 @@ public class Problem {
 //        System.out.println(Arrays.stream(dp)
 //                .mapToObj(String::valueOf)
 //                .collect(Collectors.joining(",")));
-        return -1;
+            	int result = 0;
+    	if(coins.length <= 1) {
+        	return 0;
+        }
+        else if(coins.length == 2){
+        	return coins[1];
+        	
+        }
+        else if(coins.length == 3) {
+        	return Math.max(coins[1],coins[2]);
+        }
+        else {
+        	int a1 = coins[1];
+            int a2 = coins[2];
+            int a3 = coins[3] + a1;
+            int p = -1;
+        	
+        	for(int i = 4;i < coins.length; i++) {
+        		p = Math.max(a1,a2) + coins[i];
+        		a1 = a2;
+        		a2 = a3;
+        		a3 = p;        		        	
+        	}
+        	result = Math.max( a2, a3);       	
+        }
+       
+    	return result;
     }
 }
